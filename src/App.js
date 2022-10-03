82c54a3918ccee5ba94225bfb79ad27dd962af4b
MD EJAZ ALAM <108115025+ejaz-cedcoss@users.noreply.github.com>
2022-10-04 00:17:56 +0530

    asyn await callback promises

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+    let Data = [
+     {name:"ajay",profession:"hr"},
+     {name:"vijay",profession:"hr"},
+     {name:"sanjay",profession:"hr"}
+    ]
+  
+    const getData = () =>{
+
+      setTimeout(()=>{
+        let output = "";
+        Data.forEach((val)=>{
+           output+= `<li>${val.name}</li>`
+        });
+       document.body.innerHTML = output;
+      },1000)
+    }
+
+  
+    const createData = (obj) =>{
+
+      return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+          Data.push(obj);
+          let error = false;
+          if(!error){
+            resolve();
+          }
+          else{
+            reject("kuch sahi nahi hai ....");
+          }
+        },2000)
+      })
+    
+     
+    }
+      // createData({name:"reshma",profession:"model"})
+      // .then(getData)
+      // .catch(err=>console.log(err)); 
+      // getData();
+
+
+      // ASYNC AWAIT 
+
+      const Start = async() =>{
+           await createData({name:"reshma",profession:"model"});
+           getData();
+      }
+      Start();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
     </div>
   );
 }
